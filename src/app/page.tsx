8b88175b7dcd63083eed04223d40736762b4f2ca
rabia-rana <shayanrana696@gmail.com>
2024-12-09 23:32:15 +0500

    UI/UIX Hackathon
    This is ecommerce website build by Next-js and Tailliwwind CSS

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Compony from './components/Compony'
+import Products from './components/Products'
+import Category from './components/Category'
+import { Footer } from './components/Footer'
+import Chairs from './components/Chairs'
+import Lastproducts from './components/Lastproducts'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <Compony/>
+      <Products/>
+      <Category/>
+      <Chairs/>
+      <Lastproducts/>
+      
+    </div>
+  )
+}
+
+export default Home
